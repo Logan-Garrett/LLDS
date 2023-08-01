698c6f02bdf731772407feaba6b96e20b56eff6e
Logan Garrett <ninetailsoftware@gmail.com>
2023-07-31 21:39:25 -0400

    Saving work for now.

--- circularLinkedList.c
+++ circularLinkedList.c
@@ -6,7 +6,41 @@ struct Node {
     struct Node *nextNode;
 };
 
+int lengthOfNodeList(struct Node** head) {
+    struct Node* cursor = *head;
+    int count = 0;
+
+    while (cursor != NULL) {
+        count++;
+        cursor=cursor->nextNode;
+    }
+
+    printf("\nThe Length of the Node List: ");
+    printf("%d", count);
+    return count;    
+}
+
+void insertNodeAtFront(struct Node** head, int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    // Assign info to the Node Struct.
+    newNode->data=data;
+    newNode->nextNode=*head;
+
+    if (*head == NULL) {
+        printf("I am NULL");
+    }
+
+    *head = newNode;
+}
+
 int main() {
-    printf("Hello World!");
+    struct Node* head = NULL;
+    
+    // Insert At The Front of Circular Linked List.
+    insertNodeAtFront(&head, 0);
+
+    lengthOfNodeList(&head);
+
     return 0;
 }
