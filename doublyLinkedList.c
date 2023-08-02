071e2283526ac910fc39c359064bb577064e32d2
Logan Garrett <ninetailsoftware@gmail.com>
2023-08-01 21:46:16 -0400

    Started Doubly Linked List Insert.

--- doublyLinkedList.c
+++ doublyLinkedList.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    int data;
+    struct Node* nextNode;
+    struct Node* prevNode;
+};
+
+void insertNode(struct Node** head, int data) {
+    printf("Inserted Node");
+}
+
 int main() {
-    printf("Hellow and welcome to Doubly Linked List!");
+    struct Node* head = NULL;
+    
+    insertNode(&head, 3);
+
     return 0;
 }
